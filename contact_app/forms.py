319d745a37a1acf48981fd761dd83da329a520ab
Akila27-18 <akila271819@gmail.com>
2025-07-31 23:24:38 +0530

    first commit

--- contact_app/forms.py
+++ contact_app/forms.py
@@ -12,8 +12,19 @@ class ContactModelForm(forms.ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}),
         }
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name.isdigit():
+            raise forms.ValidationError("Name cannot be all numbers.")
+        return name
 
 class ContactManualForm(forms.Form):
     name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your Name'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your Email'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message'}))
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name.isdigit():
+            raise forms.ValidationError("Name cannot be all numbers.")
+        return name
